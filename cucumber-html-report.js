60f4771348cca02664d8fef1a232c4e8f20bbeb4
Asif Biswas <asifbiswas85@gmail.com>
2022-05-22 15:42:13 +0600

    some changes

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,16 +1,31 @@
+const fs = require('fs');
+function getDirectories(path) {
+    return fs.readdirSync(path).filter(function (file) {
+      return fs.statSync(path+'/'+file).isDirectory();
+    });
+  }
+  
+  // print all the directories in the 'cypress/cucumber-html-report' folder
+    getDirectories('./reports').forEach(function (folder) {
+        console.log(folder);
+    });
+
+    //fs.rmSync(dir, { recursive: true, force: true });
+    
+
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
-jsonDir: "cypress/cucumber-json",  // ** Path of .json file **//
-reportPath: "./reports/cucumber-htmlreport.html",
-metadata: {
-browser: {
-name: "chrome",
-version: "81",
-},
-device: "Local test machine",
-platform: {
-name: "mac",
-version: "Catalina",
-},
-},
+    jsonDir: "cypress/cucumber-json",  // ** Path of .json file **//
+    reportPath: "./reports/cucumber-htmlreport.html",
+    metadata: {
+        browser: {
+            name: "chrome",
+            version: "81",
+        },
+        device: "Local test machine",
+        platform: {
+            name: "mac",
+            version: "Catalina",
+        },
+    },
 });
--- cypress/integration/automationTest/bookADemo/bookADemo.js
+++ cypress/integration/automationTest/bookADemo/bookADemo.js
@@ -2,8 +2,6 @@ import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
     return false
 })
 
